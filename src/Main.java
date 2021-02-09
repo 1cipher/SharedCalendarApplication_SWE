edff8b468fe381773d55343fa500e743b1c780ef
marco <marco.guerra@stud.unifi.it>
2021-02-09 21:28:30 +0100

    Test Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
      public static void main(String args[]) throws SQLException, IOException {
