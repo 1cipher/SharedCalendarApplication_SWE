e1932e391906c178ba03c3d891623e9a3735d935
1cipher <alessio.bonacchi@stud.unifi.it>
2021-03-13 09:30:36 +0100

    fix

---
+++ src/test/MainTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class MainTest {
+
+    public static void main(String[] args){
+        Result result = JUnitCore.runClasses(JUnitTests.class);
+        for(Failure failure:result.getFailures()) {
+
+            System.out.println(failure.toString());
+        }
+        System.out.println("Result=="+result.wasSuccessful());
+    }
+}
